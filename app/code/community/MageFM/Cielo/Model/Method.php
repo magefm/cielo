11282e56ea041700c43ce3eaac9767d664f7a1a0
Rafael Kassner <kassner@gmail.com>
2014-03-10 17:22:30 -0300

    initial

---
+++ app/code/community/MageFM/Cielo/Model/Method.php
@@ -0,0 +1,33 @@
+<?php
+
+class MageFM_Cielo_Model_Method extends Mage_Payment_Model_Method_Cc
+{
+
+    protected $_code = 'magefm_cielo';
+    protected $_isGateway = true;
+    protected $_canAuthorize = true;
+    protected $_canCapture = true;
+    protected $_canVoid = true;
+    protected $_canUseInternal = true;
+    protected $_canUseCheckout = true;
+
+    public function authorize(Varien_Object $payment, $amount)
+    {
+        die(__METHOD__);
+    }
+
+    public function capture(Varien_Object $payment, $amount)
+    {
+        die(__METHOD__);
+    }
+
+    public function void(Varien_Object $payment)
+    {
+        die(__METHOD__);
+    }
+
+    public function cancel(Varien_Object $payment)
+    {
+        die(__METHOD__);
+    }
+}
